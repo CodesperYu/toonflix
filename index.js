365f95b5ebc40fc7ba0bd3d2c831a2d1577a7e92
Jasper Yu <Jasperyu.kk@gmail.com>
2018-03-14 14:45:17 -0700

    add navigation

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 console.log(__dirname + '/css');
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
-	//__dirname : It will resolve to your project folder.
+	// __dirname : It will resolve to your project folder.
 });
 var port = process.env.PORT || 3000;
 
